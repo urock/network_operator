99979e73c6918b28981c8092e563f9323614a542
KostyaYamshanov <k.yamshanov@fastsense.tech>
2022-09-22 22:01:49 +0700

    kostya wip

--- lib/reader.cpp
+++ lib/reader.cpp
@@ -14,24 +14,40 @@ int main()
 ptree pt;
 read_xml("24_NOP_461", pt);
 
-std::cout<<"OPEN FILE"<<std::endl;
+std::vector<int> Psi_1d;
+std::vector<std::vector<int>> Psi_2d(24, std::vector<int> (24, 0)); 
 
-// std::string text = pt.get<std::string>("CONFIG.grid.content.cells.cell1.text");
+std::string column;
+std::string row;
 
-// std::cout<<text<<std::endl;
+auto test = pt.get_child("CONFIG.grid.content").front();
+std::cout<<test.first<<std::endl;
+std::cout<<test.second.get("<xmlattr>.cellcount", "")<<std::endl;
 
 BOOST_FOREACH(ptree::value_type & child, pt.get_child("CONFIG.grid.content.cells"))
 {
-	// std::cout<<"NODE NAME: "<<child.first << std::endl;
-	//if (child.first == "cell126")
-	//{
-		auto column = child.second.get("<xmlattr>.column", "test");
-		auto row = child.second.get("<xmlattr>.row", "test");
-		auto value = child.second.get("<xmlattr>.row", "test");
-		std::cout<< "column " << column << " row " << row << " value " << value <<std::endl;
-	//}  
-	// std::cout<<"test: "<<child.second.data() << std::endl;
-	// std::cout<<"TEST: "<<child.second.get<int>("<xmlattr>.column") << std::endl;
+	column = child.second.get("<xmlattr>.column", "");
+	row = child.second.get("<xmlattr>.row", "");
+	std::string value = child.second.get("<xmlattr>.text", "");
+	
+	// skip first column 
+	if(column != "0" && row != "0" && column != "" && row != "")
+	{
+		int val = std::stoi(value);
+		int m = std::stoi(column) - 1;
+		int n = std::stoi(row) - 1;
+		// std::cout<<"val "<<val<<" m "<<m<<" n "<<n<<std::endl;
+		Psi_2d[n][m] = val;
+	}
+}
+
+for(auto n : Psi_2d)
+{
+	for(auto m : n)
+	{
+		std::cout<<" "<<m<<" ";
+	}
+	std::cout<<std::endl;
 }
 
 }
