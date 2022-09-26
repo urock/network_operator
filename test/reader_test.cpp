f9d82de5e60701683f6ce106a73fa19508079e4b
KostyaYamshanov <k.yamshanov@fastsense.tech>
2022-09-26 22:46:30 +0700

    fixed reader test

--- test/reader_test.cpp
+++ test/reader_test.cpp
@@ -1,12 +1,30 @@
 #include "reader.h"
-
+#include <iostream>
+#include <string> 
 #include <gtest/gtest.h>
 
+#include <limits.h>
+#include <unistd.h>
 
+namespace
+{
+
+std::string getexepath()
+{
+  char result[ PATH_MAX ];
+  ssize_t count = readlink( "/proc/self/exe", result, PATH_MAX );
+  return std::string( result, (count > 0) ? count : 0 );
+}
+
+} // namespace
 
 TEST(Reader, ConstructorTests)
 {
-    NOPMatrixReader reader = NOPMatrixReader("test_data/24_NOP_461", "test_data/q_461.txt");
+    std::string cwd = getexepath();
+    cwd = std::string(cwd.begin(), cwd.end()-9);
+    std::string matrixPath = cwd + "/test_data/24_NOP_461";
+    std::string paramsPath = cwd + "/test_data/q_461.txt";
+    NOPMatrixReader reader = NOPMatrixReader(matrixPath, paramsPath);
     reader.print();
 }
 
